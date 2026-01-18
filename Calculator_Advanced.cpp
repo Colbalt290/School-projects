56e44919e9196395b2d3c44293fbf1372ffb8f35
Colbalt290 <psalm.abas@gmail.com>
2026-01-18 20:24:20 +0800

    Needs updating

--- Calculator_Advanced.cpp
+++ Calculator_Advanced.cpp
@@ -6,33 +6,42 @@ using namespace std;
 int main (){
     bool start = true;
     bool Calcrun;
+    char ynh, i;
 
-while (start){
     cout << "Welcome to Vekkir's primitive calculator." << endl;
-    cout << "Would you like to begin? [y/n] or view the help menu? [h]: "
+    cout << "Would you like to begin? [y/n] or view the help menu? [h]: ";
     cin >> ynh;
 
-switch (ynh){
-    case 'y':
+while (start){
+    switch (ynh){
+        case 'y':
         cout << "Insert your numbers and operators." << endl;
         Calcrun = true;
         start = false;
         break;
-    case 'n':
+    
+        case 'n':
         cout << "See you around then.";
         break;
-    case 'h'
-        cout << "Instructions:\n 
-            1. Insert any number\n 
-            2. Insert an operator\n 
-            3. When satisfied with input, enter '=' to calculate." 
-            << endl;
-        cout << "To begin, enter 'y'";
+    
+        case 'h':
+            cout << "Instructions:\n"
+                 << "1. Insert any number\n"
+                 << "2. Insert an operator\n"
+                 << "3. When satisfied with input, enter '=' to calculate.\n" 
+                 << endl;
+            
+            cout << "To begin, enter 'y'" << endl;
+            Calcrun = true;
+            start = false;
+            
 }
-}
+    }
 
 while (Calcrun){
     cout << "This is true.";
+    cin >> i;
+    break;
 }
 
 }
