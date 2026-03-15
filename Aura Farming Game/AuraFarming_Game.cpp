63d6ab4f8a93b6be802add6a42c7cf2349c0d8ea
Colbalt290 <psalm.abas@gmail.com>
2026-03-15 16:02:33 +0800

    Organization

--- Aura Farming Game/AuraFarming_Game.cpp
+++ Aura Farming Game/AuraFarming_Game.cpp
@@ -8,12 +8,15 @@ using namespace std;
 
 void intro(){
     cout << "Welcome to Aurion, where your Aura determines your place in this world..." <<  endl;
+    delay (1);
     cout << "This world will not treat you kindly, you must grow and cultivate your Aura to mog your enemies." << endl;
 }
 
 void jobSelect(){
-  cout << "Select your Class." << endl;
+int choice;
+  cout << "Tell me... How do you wish to live in this world?" << endl;
   cout << " 1. Student \n 2. Professor \n 3. Master \n 4. Tarnished" << endl;
+  cin >> choice;
 }
 int main(){
     
